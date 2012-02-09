1bcede1950c8c77df91ec5317de1fc5182dee556
hhofer69 <hhofer69@635c5b45-4c9d-cfb5-559e-c3b250a1a487>
2012-02-09 10:57:13 +0000

    * [utils] flush log files after writing * [options] cosmetics * [build] Slowly start adding unit tests * [build] [changemon] remove resource directory from AMO build

--- trunk/amoexit.py
+++ trunk/amoexit.py
@@ -5,13 +5,14 @@ if item.filename == "chrome/content/gpchangemon.jsm":
   f = file("chrome/content/gpnullmon.jsm")
   xpidata = f.read()
   f.close()
-elif item.filename == "chrome/content/gpnullmon.jsm":
-  xpidata = None
-elif item.filename == "chrome/content/gpcmdatabase.jsm":
-  xpidata = None
-elif item.filename == "chrome/resource/changemon/changemon.sql":
-  xpidata = None
-elif item.filename == "chrome/resource/changemon/cmviews.sql":
+elif item.filename in [
+  "chrome/content/gpnullmon.jsm",
+  "chrome/content/gpcmdatabase.jsm",
+  "chrome/resource/changemon/changemon.sql",
+  "chrome/resource/changemon/cmviews.sql",
+  "chrome/resource/changemon/",
+  "changelog.txt",
+  ]:
   xpidata = None
 elif item.filename == "chrome.manifest":
   amo_r = re.compile(r"\s*resource\s+gpchangemon.*?\n", re.S) # 2.6 compat
